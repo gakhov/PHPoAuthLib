20dae3126ace4de1fc6cea95346e162cd0a02d5c
gakhov <andrii.gakhov@gmail.com>
2016-07-17 15:51:49 +0200

    Add scope and fix test for FitBit

--- tests/Unit/OAuth2/Service/FitBitTest.php
+++ tests/Unit/OAuth2/Service/FitBitTest.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace OAuthTest\Unit\OAuth2\Service;
+
 use OAuth\OAuth2\Service\FitBit;
 use OAuth\Common\Token\TokenInterface;
+
 class FitBitTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -14,8 +17,10 @@ class FitBitTest extends \PHPUnit_Framework_TestCase
             $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
             $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
         );
+
         $this->assertInstanceOf('\\OAuth\\OAuth2\\Service\\ServiceInterface', $service);
     }
+
     /**
      * @covers OAuth\OAuth2\Service\FitBit::__construct
      */
@@ -26,8 +31,10 @@ class FitBitTest extends \PHPUnit_Framework_TestCase
             $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
             $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
         );
+
         $this->assertInstanceOf('\\OAuth\\OAuth2\\Service\\AbstractService', $service);
     }
+
     /**
      * @covers OAuth\OAuth2\Service\FitBit::__construct
      */
@@ -40,8 +47,10 @@ class FitBitTest extends \PHPUnit_Framework_TestCase
             array(),
             $this->getMock('\\OAuth\\Common\\Http\\Uri\\UriInterface')
         );
+
         $this->assertInstanceOf('\\OAuth\\OAuth2\\Service\\AbstractService', $service);
     }
+
     /**
      * @covers OAuth\OAuth2\Service\FitBit::__construct
      * @covers OAuth\OAuth2\Service\FitBit::getAuthorizationEndpoint
@@ -53,8 +62,10 @@ class FitBitTest extends \PHPUnit_Framework_TestCase
             $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
             $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
         );
+
         $this->assertSame('https://www.fitbit.com/oauth2/authorize', $service->getAuthorizationEndpoint()->getAbsoluteUri());
     }
+
     /**
      * @covers OAuth\OAuth2\Service\FitBit::__construct
      * @covers OAuth\OAuth2\Service\FitBit::getAccessTokenEndpoint
@@ -66,8 +77,78 @@ class FitBitTest extends \PHPUnit_Framework_TestCase
             $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface'),
             $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
         );
+
         $this->assertSame('https://api.fitbit.com/oauth2/token', $service->getAccessTokenEndpoint()->getAbsoluteUri());
     }
+
+    /**
+     * @covers OAuth\OAuth2\Service\FitBit::__construct
+     * @covers OAuth\OAuth2\Service\FitBit::getAuthorizationMethod
+     */
+    public function testGetAuthorizationMethod()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnArgument(2));
+
+        $token = $this->getMock('\\OAuth\\OAuth2\\Token\\TokenInterface');
+        $token->expects($this->once())->method('getEndOfLife')->will($this->returnValue(TokenInterface::EOL_NEVER_EXPIRES));
+        $token->expects($this->once())->method('getAccessToken')->will($this->returnValue('foo'));
+
+        $storage = $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface');
+        $storage->expects($this->once())->method('retrieveAccessToken')->will($this->returnValue($token));
+
+        $service = new FitBit(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $storage
+        );
+
+        $headers = $service->request('/users/28H22H/profile.json');
+
+        $this->assertTrue(array_key_exists('Authorization', $headers));
+        $this->assertTrue(in_array('Bearer foo', $headers, true));
+    }
+
+    /**
+     * @covers OAuth\OAuth2\Service\FitBit::__construct
+     * @covers OAuth\OAuth2\Service\FitBit::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseThrowsExceptionOnNulledResponse()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue(null));
+
+        $service = new FitBit(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestAccessToken('foo');
+    }
+
+    /**
+     * @covers OAuth\OAuth2\Service\FitBit::__construct
+     * @covers OAuth\OAuth2\Service\FitBit::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseThrowsExceptionOnErrorDescription()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('error_description=some_error'));
+
+        $service = new FitBit(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
+        );
+
+        $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
+        $service->requestAccessToken('foo');
+    }
+
     /**
      * @covers OAuth\OAuth2\Service\FitBit::__construct
      * @covers OAuth\OAuth2\Service\FitBit::parseAccessTokenResponse
@@ -76,28 +157,51 @@ class FitBitTest extends \PHPUnit_Framework_TestCase
     {
         $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
         $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('error=some_error'));
+
         $service = new FitBit(
             $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
             $client,
             $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
         );
+
         $this->setExpectedException('\\OAuth\\Common\\Http\\Exception\\TokenResponseException');
+
         $service->requestAccessToken('foo');
     }
+
     /**
      * @covers OAuth\OAuth2\Service\FitBit::__construct
      * @covers OAuth\OAuth2\Service\FitBit::parseAccessTokenResponse
-     * @covers OAuth\OAuth2\Service\FitBit::requestAccessToken
      */
-    public function testParseAccessTokenResponseValid()
+    public function testParseAccessTokenResponseValidWithoutRefreshToken()
     {
         $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
-        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('access_token=foo'));
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"access_token":"foo","expires_in":"bar"}'));
+
         $service = new FitBit(
             $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
             $client,
             $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
         );
+
+        $this->assertInstanceOf('\\OAuth\\OAuth2\\Token\\StdOAuth2Token', $service->requestAccessToken('foo'));
+    }
+
+    /**
+     * @covers OAuth\OAuth2\Service\FitBit::__construct
+     * @covers OAuth\OAuth2\Service\FitBit::parseAccessTokenResponse
+     */
+    public function testParseAccessTokenResponseValidWithRefreshToken()
+    {
+        $client = $this->getMock('\\OAuth\\Common\\Http\\Client\\ClientInterface');
+        $client->expects($this->once())->method('retrieveResponse')->will($this->returnValue('{"access_token":"foo","expires_in":"bar","refresh_token":"baz"}'));
+
+        $service = new FitBit(
+            $this->getMock('\\OAuth\\Common\\Consumer\\CredentialsInterface'),
+            $client,
+            $this->getMock('\\OAuth\\Common\\Storage\\TokenStorageInterface')
+        );
+
         $this->assertInstanceOf('\\OAuth\\OAuth2\\Token\\StdOAuth2Token', $service->requestAccessToken('foo'));
     }
 }
